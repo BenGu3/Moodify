79d9bd070bd281cfb5d6644389066c3b851fa56e
BenGu3 <benjee.012@gmail.com>
2016-12-21 22:13:11 -0500

    Added in $http.post to create a playlist. Ran into the problem that the authcode must be given by the user, so I have to look into user authentication before continuing with the playlist creating and adding.

--- Moodify.js
+++ Moodify.js
@@ -4,6 +4,7 @@ app.controller('MoodifyController', function($scope, $http) {
 
     $scope.searchresults = [];
     $scope.stations = [];
+    $scope.tracks = [];
 
 
     $scope.fetch = function () {
@@ -16,7 +17,7 @@ app.controller('MoodifyController', function($scope, $http) {
         for(var i = 0; i < $scope.stations.length; i++)
         {
             if ($scope.stations[i].name == stationname) {
-                checkstation = true;
+                checkstation = true;a
             }
         }
 
@@ -25,14 +26,39 @@ app.controller('MoodifyController', function($scope, $http) {
             $scope.stations.push({
                 name: stationname
             });
+
+
+// Create New Playlist
+            $http.post("https://api.spotify.com/v1/users/bengu3/playlists",
+            {
+
+                headers: {
+                    "Accept": 'application/json',
+                    "Authorization": 'Bearer BQAuaE-s2_EBCB6gycxj-DN4QqRELQhUs42XfoyACn1pJZNrK1jKnYZ0nlF9koxUqyurMcyqULVPW1QU_JHBwXRZs_z_WEZ_XAWYkHeZjIKHFFMSsgxvarKW1LENK2a5L1Kf1u4XhqG4kiadv67X1BzIJ5Hja0oLeyNzCJifbCDUHeI0UfyiyzJx8nUTfjPIT80_7wjc51Lp-U34xP1ocMzGU1IHyQQOBsuPgWznQDhC-A'
+                    // "Content-Type": 'application/json'
+                },
+
+                data: {
+                    "name": "NewPlaylist",
+                    "public": false
+                }
+
+            })
+            .then(function(result){
+                console.log(result);
+            });
         
 
+
+
+
+// Get music
             $http.get("https://api.spotify.com/v1/search?q=" + $scope.query + "&type=album")
             .then(function(response)
             { 
 
                 var albumurl = response.data.albums.items[0].href;
-                console.log(albumurl);
+                //console.log(albumurl);
 
 
                 $http.get(albumurl)
@@ -41,34 +67,53 @@ app.controller('MoodifyController', function($scope, $http) {
 
                     for (var i = 0; i < albumresponse.data.tracks.items.length; i++) {
 
-                        var trackname = albumresponse.data.tracks.items[i].name;
-                        var previewurl = albumresponse.data.tracks.items[i].preview_url;
-                        var albumimg = albumresponse.data.images[1].url;
-                        console.log(trackname);
-                        console.log(albumimg);
-                        console.log(previewurl);
 
-
-                        $scope.searchresults.push({ 
-                            track: trackname,
-                            preview: previewurl,
-                            art: albumimg
-                        });
+                        // ****ORIGINAL TEST CODE****
+                        // Got track name, album image, and 30 seconds preview ad displayed those.
+                        // ---------------------------------------------------------------
+                        //
+                        // var trackname = albumresponse.data.tracks.items[i].name;
+                        // var previewurl = albumresponse.data.tracks.items[i].preview_url;
+                        // var albumimg = albumresponse.data.images[1].url;
+                        // console.log(trackname);
+                        // console.log(albumimg);
+                        // console.log(previewurl);
+                        //
+                        //
+                        // $scope.searchresults.push({ 
+                        //     track: trackname,
+                        //     preview: previewurl,
+                        //     art: albumimg
+                        // });
 
 
                         var featuresurl = 'https://api.spotify.com/v1/audio-features/' + albumresponse.data.tracks.items[i].id;
-                        console.log(featuresurl);
+                        //console.log(featuresurl);
 
                         $http.get(featuresurl, {
                             headers: {
-                                "Authorization": 'Bearer BQAYas0yik9rmTXooT5I2QulzLF2BUIgw3v5lyr3kuSq6-X09kEDw-DLLrt5QX0mJs-PAaO0fzNDGDiqOgdPXDVfLEFP-JJohMA3Iv73hLyn5PywftlMcUcKLKOiO-keSKdmItYzfAjBtK0NM8u-nYBMlGtTlnNujcjcWKf_1gRkyvh2HG0dKCUQ4YGCrBN5iA'
+                                "Authorization": 'Bearer BQA0YQRuCu5sQtvtuALAmrNJ4pVwXTaHP2rrjtowR5baQ_nKiktC2-KEFjXxp0Bi0_YvYoiF75Eh4jByS8ln_mm4z8WFji817TgO5oGfIUaLWnf2TOFthPtWy2Va99nFsambeQ1X3asgzQBUzM20TWA2WpSJ4eMGL68mNzZTBtsl5SHDFBpM6eGV7AIVV7SP1ojgUL1_YrnKELAnFITllKDIZ3baSrr-yYIOxMSCJr5vag'
                             }
                           })
                         .then(function(res){
-                            console.log(res)
+                            //console.log(res)
                             var valence = res.data.valence;
+                            var tracklink = res.data.uri;
 
-                            console.log(valence);                            
+                            // console.log(tracklink);
+                            // console.log(valence);
+
+                            if(valence > .65)
+                            {
+                                $scope.tracks.push({
+                                    uri: tracklink,
+                                    valence: valence
+                                });
+
+                                console.log("Added");
+                            }
+
+
                           });
                     }
                 });
