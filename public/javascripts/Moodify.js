74a48e239becf1060822fbbbc66a89f0c28af482
BenGu3 <benjee.012@gmail.com>
2017-01-03 16:50:34 -0500

    initial .css added

--- public/javascripts/Moodify.js
+++ public/javascripts/Moodify.js
@@ -21,7 +21,7 @@ angular
     var start_up = function () {
         $window.loading_screen = $window.pleaseWait({
             logo: "",
-            backgroundColor: '#1DB954',
+            backgroundColor: '#dd944a',
             loadingHtml: "<div class=\"sk-cube-grid\"><div class=\"sk-cube sk-cube1\"></div><div class=\"sk-cube sk-cube2\"></div><div class=\"sk-cube sk-cube3\"></div><div class=\"sk-cube sk-cube4\"></div><div class=\"sk-cube sk-cube5\"></div><div class=\"sk-cube sk-cube6\"></div><div class=\"sk-cube sk-cube7\"></div><div class=\"sk-cube sk-cube8\"></div><div class=\"sk-cube sk-cube9\"></div></div>"
         });
     }
